68eb646e13c725028ab82d476f3c728327b8e099
mcamillosf <matheuscamillo@hotmail.com.br>
2021-10-26 02:56:19 -0300

    Updates

---
+++ precache-manifest.08cfd0b393a9979f72b4a422643e3743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b12a7f72716cc0bb8711a4e5653ab4d",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "1c28a5bb85bcf3717ff8",
+    "url": "/trybewallet/static/css/main.549426a7.chunk.css"
+  },
+  {
+    "revision": "6ff6702f5c0f6ce3ffa0",
+    "url": "/trybewallet/static/js/2.72110fe0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.72110fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c28a5bb85bcf3717ff8",
+    "url": "/trybewallet/static/js/main.3070a6ec.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "665aa750e469fada9396d058febc6395",
+    "url": "/trybewallet/static/media/teste1.665aa750.jpg"
+  }
+]);
