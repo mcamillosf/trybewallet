fb5a7df75a6d95788bd8b4c0c862ed7e1f0f5dd8
mcamillosf <matheuscamillo@hotmail.com.br>
2021-10-26 02:59:19 -0300

    adicionando recursos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Login, Wallet } from './pages';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/carteira" component={ Wallet } />
-    </Switch>
+    <Router basename={ process.env.PUBLIC_URL }>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </Router>
   );
 }
 
